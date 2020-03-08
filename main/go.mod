f157946de1cbc53fd2cf5cc0c9ef5ab869b47118
SirWaithaka <kennwaithaka@gmail.com>
2020-03-08 12:50:17 +0300

    Dependencies and bins

---
+++ main/go.mod
@@ -0,0 +1,7 @@
+module github.com/sirwaithaka/simple-wallet/main
+
+go 1.13
+
+require wallet v1.0.0
+
+replace wallet => ../wallet
--- main/wallet-server.go
+++ main/wallet-server.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber"
+
+	"wallet/rest"
+)
 
 func main() {
-	fmt.Printf("Hello world")
+
+	// create the fiber server.
+	server := fiber.New()
+	rest.Router(server) // add endpoints
+
+	// listen and serve
+	port := fmt.Sprintf(":%v", 6700)
+	log.Fatal(server.Listen(port))
 }
