82c5434f1f1a4a8c7b320a3c70229d2f1a7a03d1
SirWaithaka <kennwaithaka@gmail.com>
2020-03-09 21:56:52 +0300

    Inject domain context into router

--- wallet/rest/router.go
+++ wallet/rest/router.go
@@ -2,22 +2,23 @@ package rest
 
 import (
 	"github.com/gofiber/fiber"
+	"wallet/registry"
 	"wallet/rest/api/account"
 	"wallet/rest/api/user"
 )
 
 
-func Router(fiberApp *fiber.App) {
+func Router(fiberApp *fiber.App, domain *registry.Domain) {
 
 	apiGroup := fiberApp.Group("/api")
 
-	apiRouteGroup(apiGroup)
+	apiRouteGroup(apiGroup, domain)
 }
 
 
-func apiRouteGroup(g *fiber.Group) {
-	g.Post("/login", user.Authenticate())
-	g.Post("/user", user.Register())
+func apiRouteGroup(g *fiber.Group, domain *registry.Domain) {
+	g.Post("/login", user.Authenticate(domain.User))
+	g.Post("/user", user.Register(domain.User))
 
 	g.Post("/account/deposit", account.Deposit())
 	g.Post("/account/withdrawal", account.Withdraw())
