040da72018ef58b0cbbaf2e1b3e99423c6daebeb
SirWaithaka <kennwaithaka@gmail.com>
2020-03-12 15:37:39 +0300

    Rename data types to contracts

--- wallet/data/channels.go
+++ wallet/data/channels.go
@@ -1,13 +1,34 @@
 package data
 
-import uuid "github.com/satori/go.uuid"
+import (
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
 
-type NewRegisteredUser struct {
+// UserContract describe the characteristics of data that should
+// be passed along in channels for when a user is created or something.
+type UserContract struct {
 	UserID uuid.UUID
 }
 
 type ChanNewUsers struct {
-	Channel chan NewRegisteredUser
-	Reader <-chan NewRegisteredUser
-	Writer chan<- NewRegisteredUser
+	Channel chan UserContract
+	Reader  <-chan UserContract
+	Writer  chan<- UserContract
+}
+
+// TransactionContract represents the type of data
+// required to record a new transaction in the database.
+type TransactionContract struct {
+	UserID    uuid.UUID
+	AccountID uuid.UUID
+	Amount    float64
+	TxType    string // transaction type
+	Timestamp time.Time
+}
+
+type ChanNewTransactions struct {
+	Channel chan TransactionContract
+	Reader  <-chan TransactionContract
+	Writer  chan<- TransactionContract
 }
