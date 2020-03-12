8da559697b2506590a2fd796aaed4207d332f677
SirWaithaka <kennwaithaka@gmail.com>
2020-03-12 12:12:39 +0300

    Add api handler for account deposit

---
+++ wallet/rest/api/accounts/errors.go
@@ -0,0 +1,46 @@
+package accounts
+
+import (
+	"net/http"
+	"reflect"
+	"wallet/account"
+)
+
+type ErrHTTP struct {
+	Error   string `json:"error"`
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+}
+
+func ErrResponse(err error) ErrHTTP {
+
+	switch err.(type) {
+	case account.ErrUserHasAccount:
+		return ErrHTTP{
+			Error:   reflect.TypeOf(err).Name(),
+			Message: err.Error(),
+			Status:  http.StatusBadRequest,
+		}
+
+	case account.ErrAccountAccess:
+		return ErrHTTP{
+			Error:   reflect.TypeOf(err).Name(),
+			Message: err.Error(),
+			Status:  http.StatusForbidden,
+		}
+
+	case account.ErrAmountBelowMinimum:
+		return ErrHTTP{
+			Error:   reflect.TypeOf(err).Name(),
+			Message: err.Error(),
+			Status:  http.StatusBadRequest,
+		}
+
+	default:
+		return ErrHTTP{
+			Error:   reflect.TypeOf(err).Name(),
+			Message: err.Error(),
+			Status:  500,
+		}
+	}
+}
--- wallet/rest/api/accounts/handler.go
+++ wallet/rest/api/accounts/handler.go
@@ -1,16 +1,39 @@
-package account
+package accounts
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber"
+	uuid "github.com/satori/go.uuid"
 	"wallet/account"
 )
+
+type param struct {
+	Amount uint `json:"amount"`
+}
 
 // Deposit allows user to deposit or credit their
 // account.
 func Deposit(interactor account.Interactor) func(*fiber.Ctx) {
 
 	return func(ctx *fiber.Ctx) {
+		var userDetails = ctx.Locals("userDetails").(map[string]string)
+		userId := userDetails["userId"]
 
+		var p param
+		_ = ctx.BodyParser(&p)
+
+		balance, err := interactor.Deposit(uuid.FromStringOrNil(userId), p.Amount)
+		if err != nil {
+			errHTTP := ErrResponse(err)
+			_ = ctx.Status(errHTTP.Status).JSON(errHTTP)
+			return
+		}
+
+		_ = ctx.JSON(map[string]interface{}{
+			"message": fmt.Sprintf("Amount successfully deposited new balance %v", balance),
+			"balance": balance,
+			"userId": userId,
+		})
 	}
 }
 
