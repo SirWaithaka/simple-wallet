316e8a469d7d0f09993390be66bd44eec7dfe495
SirWaithaka <kennwaithaka@gmail.com>
2020-03-12 11:51:00 +0300

    Pluralize the api domain handler packages

--- wallet/rest/api/accounts/handler.go
+++ wallet/rest/api/accounts/handler.go
@@ -1,27 +1,27 @@
 package account
 
-import "github.com/gofiber/fiber"
-
-
-// Withdraw allows user to withdraw or debit their
-// account.
-func Withdraw() func(*fiber.Ctx) {
-
-	return func(ctx *fiber.Ctx) {
-
-	}
-}
-
+import (
+	"github.com/gofiber/fiber"
+	"wallet/account"
+)
 
 // Deposit allows user to deposit or credit their
 // account.
-func Deposit() func(*fiber.Ctx) {
+func Deposit(interactor account.Interactor) func(*fiber.Ctx) {
 
 	return func(ctx *fiber.Ctx) {
 
 	}
 }
 
+// Withdraw allows user to withdraw or debit their
+// account.
+func Withdraw(interactor account.Interactor) func(*fiber.Ctx) {
+
+	return func(ctx *fiber.Ctx) {
+
+	}
+}
 
 // MiniStatement returns a small short summary of the
 // most recent transactions on an account.
