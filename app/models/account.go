712d50337e0f7b2115284231c5f43151f44730b7
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-14 17:55:27 +0300

    Add types for accounts and states

--- app/models/account.go
+++ app/models/account.go
@@ -6,26 +6,34 @@ import (
 	"github.com/gofrs/uuid"
 )
 
+// AccountStatus (active,dormant,frozen,suspended)
+type AccountStatus string
+
+// AccountType (savings,current,utility)
+type AccountType string
+
 const (
-	StatusActive    = "active"
-	StatusFrozen    = "frozen"
-	StatusSuspended = "suspended"
+	StatusActive    = AccountStatus("active")
+	StatusDormant   = AccountStatus("dormant")
+	StatusFrozen    = AccountStatus("frozen")
+	StatusSuspended = AccountStatus("suspended")
 )
 
 const (
 	// different types of accounts a user could hold
 	// we will use current account only.
-	TypeSavings = "savings"
-	TypeCurrent = "current"
-	TypeUtility = "utility"
+	TypeSavings = AccountType("savings")
+	TypeCurrent = AccountType("current")
+	TypeUtility = AccountType("utility")
 )
 
+// Account entity definition
 type Account struct {
-	ID          uuid.UUID `json:"accountId"`
-	Balance     float64   `json:"balance"`
-	Status      string    `json:"status"`
-	AccountType string    `json:"accountType"`
-	UserID      uuid.UUID `json:"userId"`
+	ID          uuid.UUID     `json:"accountId"`
+	Balance     float64       `json:"balance"`
+	Status      AccountStatus `json:"status"`
+	AccountType AccountType   `json:"accountType"`
+	UserID      uuid.UUID     `json:"userId"`
 
 	CreationDate    time.Time `json:"creationDate"`
 	LastUpdatedDate time.Time `json:"lastUpdatedDate"`
