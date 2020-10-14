b18b6e0fc7f597969468fe2706897725586ad8ff
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-14 17:54:22 +0300

    Update entity add hook; Add properties

--- app/models/user.go
+++ app/models/user.go
@@ -2,20 +2,24 @@ package models
 
 import (
 	"github.com/gofrs/uuid"
+	"gorm.io/gorm"
 )
 
-// Names according to national id/passport,
-// id/passport no, D.O.B, creation date,
-// last update date, email,phone, Password/PIN,
-// Country (So you need a country table),
-// reset pin/pass count, max reset pin/pass count,
-// account status(active,dormant,frozen,suspended etc),etc
+// User entity definition
 type User struct {
-	ID          uuid.UUID `json:"userId" gorm:"primary_key"`
-	FirstName   string    `json:"firstName"`
-	LastName    string    `json:"lastName"`
-	Email       string    `json:"email" gorm:"not null;unique"`
-	PhoneNumber string    `json:"phoneNumber" gorm:"not null;unique"`
-	PassportNo  string    `json:"passportNumber"`
-	Password    string    `json:"password" gorm:"not null"`
+	gorm.Model // embed created_at, deleted_at, updated_at
+
+	ID          uuid.UUID `gorm:"primarykey"`
+	FirstName   string
+	LastName    string
+	Email       string `gorm:"not null;unique"`
+	PhoneNumber string `gorm:"not null;unique"`
+	PassportNo  string
+	Password    string `gorm:"not null"`
+}
+
+// BeforeCreate hook will be used to add uuid to entity before adding to db
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	u.ID, _ = uuid.NewV4()
+	return nil
 }
