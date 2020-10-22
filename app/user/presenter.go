7ffbd431418bb0c7f9811308ed44a5e9ae9db92e
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-22 22:35:26 +0300

    Moved model

--- app/user/presenter.go
+++ app/user/presenter.go
@@ -5,18 +5,13 @@ import (
 	"simple-wallet/app/models"
 )
 
-type SignedUser struct {
-	UserID string `json:"userId"`
-	Token string `json:"token"`
-}
-
 func RegistrationResponse(user *models.User) map[string]interface{} {
 	return map[string]interface{}{
-		"status": "success",
+		"status":  "success",
 		"message": "user created",
-		"user": map[string]interface{} {
+		"user": map[string]interface{}{
 			"userId": user.ID,
-			"email": user.Email,
+			"email":  user.Email,
 		},
 	}
 }
