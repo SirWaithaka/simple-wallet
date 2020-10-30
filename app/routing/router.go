1344cda73b7222b2e14aa6eb32a3238cb4569875
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-30 11:20:47 +0300

    Use new dir structure

--- app/routing/router.go
+++ app/routing/router.go
@@ -3,9 +3,9 @@ package routing
 import (
 	"simple-wallet/app"
 	"simple-wallet/app/registry"
-	"simple-wallet/app/routing/handlers"
-	"simple-wallet/app/routing/handlers/accounts"
+	"simple-wallet/app/routing/account_handlers"
 	"simple-wallet/app/routing/middleware"
+	"simple-wallet/app/routing/user_handlers"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -21,13 +21,13 @@ func Router(fiberApp *fiber.App, domain *registry.Domain, config app.Config) {
 
 func apiRouteGroup(g fiber.Router, domain *registry.Domain, config app.Config) {
 
-	g.Post("/login", handlers.Authenticate(domain.User, config))
-	g.Post("/user", handlers.Register(domain.User))
+	g.Post("/login", user_handlers.Authenticate(domain.User, config))
+	g.Post("/user", user_handlers.Register(domain.User))
 
-	g.Get("/account/balance", middleware.AuthByBearerToken(config.Secret), accounts.BalanceEnquiry(domain.Account))
-	g.Post("/account/deposit", middleware.AuthByBearerToken(config.Secret), accounts.Deposit(domain.Account))
-	g.Post("/account/withdrawal", middleware.AuthByBearerToken(config.Secret), accounts.Withdraw(domain.Account))
-	g.Post("/account/withdraw", middleware.AuthByBearerToken(config.Secret), accounts.Withdraw(domain.Account))
+	g.Get("/account/balance", middleware.AuthByBearerToken(config.Secret), account_handlers.BalanceEnquiry(domain.Account))
+	g.Post("/account/deposit", middleware.AuthByBearerToken(config.Secret), account_handlers.Deposit(domain.Account))
+	g.Post("/account/withdrawal", middleware.AuthByBearerToken(config.Secret), account_handlers.Withdraw(domain.Account))
+	g.Post("/account/withdraw", middleware.AuthByBearerToken(config.Secret), account_handlers.Withdraw(domain.Account))
 
-	g.Get("/account/statement", middleware.AuthByBearerToken(config.Secret), accounts.MiniStatement(domain.Transaction))
+	g.Get("/account/statement", middleware.AuthByBearerToken(config.Secret), account_handlers.MiniStatement(domain.Transaction))
 }
