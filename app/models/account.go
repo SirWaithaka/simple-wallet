74921cf281a5c179394b468f16c3dbbb161840da
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-30 10:16:23 +0300

    Update balance to record cents; Add methods on account to transform balance

--- app/models/account.go
+++ app/models/account.go
@@ -28,11 +28,36 @@ const (
 
 // Account entity definition
 type Account struct {
-	gorm.Model
+	ID uuid.UUID
 
-	ID          uuid.UUID
-	Balance     float64       `gorm:"column:balance"`
+	// balance will be stored in cents
+	AvailableBalance uint `gorm:"column:available_balance"`
+
 	Status      AccountStatus `gorm:"column:status"`
 	AccountType AccountType   `gorm:"column:account_type"`
 	UserID      uuid.UUID     `gorm:"column:user_id;not null;unique"` // a user can only have one account
+
+	gorm.Model
+}
+
+// Balance converts balance from cents
+func (acc Account) Balance() float64 {
+	return float64(acc.AvailableBalance / 100)
+}
+
+// Credit add an amount to account balance and return it
+func (acc Account) Credit(amount uint) uint {
+	// convert incoming amount into cents and add to account balance
+	return (amount * 100) + acc.AvailableBalance
+}
+
+// Debit subtract an amount from account balance and return it
+func (acc Account) Debit(amount uint) uint {
+	// convert incoming amount into cents and subtract to account balance
+	return acc.AvailableBalance - (amount * 100)
+}
+
+// IsBalanceLessThanAmount converts amount into cents and returns true if balance is less than amount
+func (acc Account) IsBalanceLessThanAmount(amount uint) bool {
+	return acc.AvailableBalance < (amount * 100)
 }
