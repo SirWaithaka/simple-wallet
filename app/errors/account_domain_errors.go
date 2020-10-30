11bc98aadfadac406e8c69ae7f594a97f24904d9
Sir Waithaka <kennwaithaka@gmail.com>
2020-10-30 10:14:31 +0300

    Update account domain errors

--- app/errors/account_domain_errors.go
+++ app/errors/account_domain_errors.go
@@ -1,16 +1,22 @@
 package errors
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
 
-// ErrUserHasAccount ...
-type ErrUserHasAccount struct {
-	message   string
-	AccountId string
-	UserId    string
-}
+	"github.com/gofrs/uuid"
+)
 
-func (err ErrUserHasAccount) Error() string {
-	return fmt.Sprintf("user %v has account with id %v", err.UserId, err.AccountId)
+const (
+	AccountNotCreated          = ErrorMessage("user's account has not been created, report issue")
+	DepositAmountBelowMinimum  = ErrorMessage("cannot deposit amounts less than")
+	WithdrawAmountBelowMinimum = ErrorMessage("cannot withdraw amounts less than")
+	WithdrawAmountAboveBalance = ErrorMessage("cannot withdraw amount, account balance not enough")
+)
+
+// ErrUserHasAccount
+func ErrUserHasAccount(userID, accountID uuid.UUID) ErrorMessage {
+	return ErrorMessage(fmt.Sprintf("user %v has account with id %v", userID, accountID))
 }
 
 // ErrAccountAccess ...
@@ -19,26 +25,32 @@ type ErrAccountAccess struct {
 	message string
 }
 
-
 func (err ErrAccountAccess) Error() string {
 	msg := fmt.Sprintf("couldn't access account. %v", err.Reason)
 	return msg
 }
 
-// ErrAmountBelowMinimum
-type ErrAmountBelowMinimum struct {
-	Message string
+// errAmountBelowMinimum
+type errAmountBelowMinimum struct {
+	MinAmount uint // minimum amount allowable for deposit or withdraw
+	Message   ErrorMessage
 }
 
-func (err ErrAmountBelowMinimum) Error() string {
-	return err.Message
+func (err errAmountBelowMinimum) Error() string {
+	return string(err.Message) + " " + strconv.Itoa(int(err.MinAmount))
+}
+
+func ErrAmountBelowMinimum(min uint, message ErrorMessage) error {
+	return errAmountBelowMinimum{MinAmount: min, Message: message}
 }
 
 // ErrNotEnoughBalance
 type ErrNotEnoughBalance struct {
-	Message string
+	Message ErrorMessage
+	Amount  uint
+	Balance float64
 }
 
 func (err ErrNotEnoughBalance) Error() string {
-	return err.Message
+	return fmt.Sprintf("%s. Amount: %v Balance: %v", string(err.Message), err.Amount, err.Balance)
 }
